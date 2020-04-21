462f818c63e22ed0657e6551dbe70557299878b1
Stephen Chong <chong@seas.harvard.edu>
2020-04-20 22:43:19 -0400

    Create files by category for quicker access

--- Unicode.lbaction/Contents/Scripts/default.js
+++ Unicode.lbaction/Contents/Scripts/default.js
@@ -8,56 +8,73 @@
 //
 
 var char_category_file = "/Contents/Resources/Categories.json";
-var unicode_data_file = "/Contents/Resources/unicode_data.json";
+var data_file_dir = "/Contents/Resources/";
 
-function convertCharData() {
-    LaunchBar.log("Converting character data from " + char_category_file);
+function filename_from_cat_chain(chain) {
+    var s = Action.path + data_file_dir + "cat";
+    for (var i = 0; i < chain.length; i++) {
+	s += "-";	
+	s += chain[i].replace(/[\W]+/g,"_");
+    }
+    
+    return s+".json";
+}
+
+function createDatafiles() {
+    LaunchBar.log("Creating data files from " + char_category_file);
     var chardata = File.readJSON(Action.path+char_category_file);
 
 
     var chldrn = [];
-    var lists_to_sort = [chldrn];
-    var dict = { children : chldrn };
-    var nav_top = { d : dict };
+    var top_category = { chain : [], items : [], categories : {} };
     for (var ind = 0; ind < chardata.length; ind++) {
 	var ch = chardata[ind];
-	var nav = nav_top;
+	var curr_cat = top_category;
 	if (ch.level1 == "Control") {
 	    continue;
 	}
+
+	var category_chain = [];
 	
 	for (var i = 1; i <= 5; i++) {
 	    var levelkey = 'level'+i;
 	    var cat = ch[levelkey];
 
 	    if (!(levelkey in ch) || cat == "") {
-		// insert the character data into d
-		nav.d.children.push(charEntry(ch));
+		// insert the character data into curr_cat
+		curr_cat.items.push(charEntry(ch));
 		break;		
 	    }
+	    
 	    // Otherwise, we navigate down one level
-	    if (!(cat in nav)) {
-		// category is not yet in the navaigation dict
-		// Create it
-		chldrn = [];
-		lists_to_sort.push(chldrn);
-		var newd = {
+	    if (!(cat in curr_cat.categories)) {
+		// category is not yet a child of curr_cat
+
+		var newchain = curr_cat.chain.slice();
+		newchain.push(cat);
+		
+		curr_cat.items.push({
 		    title: cat,
 		    label: "Category",
 		    icon: "Category.icns",
-		    children : chldrn,
-		};
-		nav.d.children.push(newd);
-		nav[cat] = { d : newd };		
+		    actionReturnsItems: true,
+		    action: "run",
+		    actionArgument: {chain:newchain},
+		});
+		curr_cat.categories[cat] = {
+		    chain : newchain,
+		    items : [],
+		    categories : {},
+		}
 	    }
-	    nav = nav[cat];
+	    curr_cat = curr_cat.categories[cat];
 	}
     }
 
 
-    // Now sort...
-    for (var i = 0; i < lists_to_sort.length; i++) {
-	lists_to_sort[i].sort(function(a, b) {
+    // Now write the files
+    function visitCats(cat) {
+	cat.items.sort(function(a, b) {
             var t1 = a.title;
             var t2 = b.title;
             var l1 = a.label;
@@ -71,28 +88,40 @@ function convertCharData() {
 	    }
 	    return t1 < t2 ? -1 : t1 > t2;
 	});
+
+	var filename = filename_from_cat_chain(cat.chain);
+
+	LaunchBar.log("Writing file " + filename);
+	File.writeJSON(cat.items, filename, {prettyPrint: false});
+	
+	for (subcat in cat.categories) {
+	    visitCats(cat.categories[subcat]);
+	}
     }
 
-    return dict.children;
+    visitCats(top_category);
 }
 
-function loadCharData() {
-    var datafile = Action.path+unicode_data_file;
+function loadCategoryData(chain) {
+    var datafile = filename_from_cat_chain(chain);
 
     if (!File.exists(datafile)) {
-	LaunchBar.log("Creating unicode data " + unicode_data_file);
-	var unicode_data = convertCharData();
-	File.writeJSON(unicode_data, datafile, {prettyPrint: false});
-	return unicode_data;
+	createDatafiles();
     }
-    else {
-	LaunchBar.log("Loading unicode data from " + unicode_data_file);
-	return File.readJSON(datafile);
+
+    if (!File.exists(datafile)) {
+	LaunchBar.alert("Could not create data files");
+	return;
     }
+    LaunchBar.log("Reading datafile " + datafile);
+    return File.readJSON(datafile);
 }
 
 function run(arg) {
-    return loadCharData();
+    if (!arg) {
+	arg = { chain : [] };
+    }
+    return loadCategoryData(arg.chain);
 
 }
 
