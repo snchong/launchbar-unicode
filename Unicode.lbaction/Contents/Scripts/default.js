ed225ac73ccbe597ca0cc58f06be47f2dc12e9bd
Stephen Chong <chong@seas.harvard.edu>
2020-04-21 22:05:07 -0400

    Searching implemented

--- Unicode.lbaction/Contents/Scripts/default.js
+++ Unicode.lbaction/Contents/Scripts/default.js
@@ -8,6 +8,7 @@
 //
 
 var char_category_file = "/Contents/Resources/Categories.json";
+var all_items_file = "/Contents/Resources/all.json";
 var data_file_dir = "/Contents/Resources/";
 
 function filename_from_cat_chain(chain) {
@@ -20,86 +21,17 @@ function filename_from_cat_chain(chain) {
     return s+".json";
 }
 
-function createDatafiles() {
-    LaunchBar.log("Creating data files from " + char_category_file);
-    var chardata = File.readJSON(Action.path+char_category_file);
-
-
-    var chldrn = [];
-    var top_category = { chain : [], items : [], categories : {} };
-    for (var ind = 0; ind < chardata.length; ind++) {
-	var ch = chardata[ind];
-	var curr_cat = top_category;
-	if (ch.level1 == "Control") {
-	    continue;
-	}
-
-	var category_chain = [];
-	
-	for (var i = 1; i <= 5; i++) {
-	    var levelkey = 'level'+i;
-	    var cat = ch[levelkey];
-
-	    if (!(levelkey in ch) || cat == "") {
-		// insert the character data into curr_cat
-		curr_cat.items.push(charEntry(ch));
-		break;		
-	    }
-	    
-	    // Otherwise, we navigate down one level
-	    if (!(cat in curr_cat.categories)) {
-		// category is not yet a child of curr_cat
-
-		var newchain = curr_cat.chain.slice();
-		newchain.push(cat);
-		
-		curr_cat.items.push({
-		    title: cat,
-		    label: "Category",
-		    icon: "Category.icns",
-		    actionReturnsItems: true,
-		    action: "run",
-		    actionArgument: {chain:newchain},
-		});
-		curr_cat.categories[cat] = {
-		    chain : newchain,
-		    items : [],
-		    categories : {},
-		}
-	    }
-	    curr_cat = curr_cat.categories[cat];
-	}
+function loadAllItems() {
+    var datafile = Action.path + all_items_file;
+    if (!File.exists(datafile)) {
+	createDatafiles();
     }
 
-
-    // Now write the files
-    function visitCats(cat) {
-	cat.items.sort(function(a, b) {
-            var t1 = a.title;
-            var t2 = b.title;
-            var l1 = a.label;
-            var l2 = b.label;
-	    // Categories go first
-	    if (l1 == "Category" && l2 != "Category") {
-		return -1;
-	    }
-	    if (l2 == "Category" && l1 != "Category") {
-		return 1;
-	    }
-	    return t1 < t2 ? -1 : t1 > t2;
-	});
-
-	var filename = filename_from_cat_chain(cat.chain);
-
-	LaunchBar.log("Writing file " + filename);
-	File.writeJSON(cat.items, filename, {prettyPrint: false});
-	
-	for (subcat in cat.categories) {
-	    visitCats(cat.categories[subcat]);
-	}
+    if (!File.exists(datafile)) {
+	LaunchBar.alert("Could not create data files");
+	return;
     }
-
-    visitCats(top_category);
+    return File.readJSON(datafile);    
 }
 
 function loadCategoryData(chain) {
@@ -113,23 +45,113 @@ function loadCategoryData(chain) {
 	LaunchBar.alert("Could not create data files");
 	return;
     }
-    LaunchBar.log("Reading datafile " + datafile);
     return File.readJSON(datafile);
 }
 
-function run(arg) {
-    if (!arg) {
-	arg = { chain : [] };
+
+
+function createDatafiles() {
+    LaunchBar.log("Creating data files from " + char_category_file);
+    var chardata = File.readJSON(Action.path+char_category_file);
+
+
+    var all_items = [];
+    var top_category = { chain : [], items : [], categories : {} };
+    for (var ind = 0; ind < chardata.length; ind++) {
+	var ch = chardata[ind];
+	var curr_cat = top_category;
+
+	for (var i = 1; i <= 5; i++) {
+	    var levelkey = 'level'+i;
+	    var cat = ch[levelkey];
+
+	    if (!(levelkey in ch) || cat == "") {
+		// insert the character data into curr_cat
+		var e = charEntry(ch,curr_cat.chain);
+		all_items.push(e);
+		curr_cat.items.push(e);
+		break;		
+	    }
+	    
+	    // Otherwise, we navigate down one level
+	    if (!(cat in curr_cat.categories)) {
+		// category is not yet a child of curr_cat
+
+		var newchain = curr_cat.chain.slice();
+		newchain.push(cat);
+
+		var c = {
+		    title: cat,
+		    label: "Category",
+		    icon: "Category.icns",
+		    actionReturnsItems: true,
+		    action: "runWithItem",
+		    actionArgument: {chain:newchain},
+		};
+		all_items.push(c);
+		curr_cat.items.push(c);
+		curr_cat.categories[cat] = {
+		    chain : newchain,
+		    items : [],
+		    categories : {},
+		}
+	    }
+	    curr_cat = curr_cat.categories[cat];
+	}
     }
-    return loadCategoryData(arg.chain);
+
+
+    var sort_fn = function(a, b) {
+        var l1 = a.label;
+        var l2 = b.label;
+	// Categories go first
+	if (l1 == "Category" && l2 != "Category") {
+	    return -1;
+	}
+	if (l2 == "Category" && l1 != "Category") {
+	    return 1;
+	}
+	if (l1 == "Category") {
+	    // Sort categories by title
+            var t1 = a.title;
+            var t2 = b.title;
+	    
+	    return t1 < t2 ? -1 : t1 > t2;
+	}
+	// Sort characters by unicode character, which is in the actionArgument field
+        var c1 = a.actionArgument;
+        var c2 = b.actionArgument;
+	    
+	return c1 < c2 ? -1 : c1 > c2;
+    };
+
+    all_items.sort(sort_fn);
+    
+    // Now write the files
+    function visitCats(cat) {
+	cat.items.sort(sort_fn);
+
+	var filename = filename_from_cat_chain(cat.chain);
+
+	LaunchBar.log("Writing file " + filename);
+	File.writeJSON(cat.items, filename, {prettyPrint: false});
+	
+	for (subcat in cat.categories) {
+	    visitCats(cat.categories[subcat]);
+	}
+    }
+
+    visitCats(top_category);
+
+    File.writeJSON(all_items, Action.path + all_items_file, {prettyPrint: false});
 
 }
 
-function charEntry(ch) {
-    var character = String.fromCodePoint(parseInt("0x"+ch.code));
+function charEntry(ch, chain) {
+    var decimal = parseInt("0x"+ch.code)
+    var character = String.fromCodePoint(decimal);
     var iconIdentifier = "character:" + character;
-    var unicode = "U+" + character.charCodeAt(0).toString(16).toUpperCase();
-    var decimal = character.charCodeAt(0).toString(10).toUpperCase();
+    var unicode = "U+" + ch.code;
     return {
         title: ch.name,
         subtitle: "Unicode " + unicode +"; decimal " + decimal,
@@ -147,8 +169,12 @@ function charEntry(ch) {
                 label: "Unicode",
             },
             {
-                title: decimal,
+                title: decimal.toString(),
                 label: "Decimal",
+            },
+            {
+                title: chain.join(" ▷ "),
+                label: "Category",
             },
             {
                 title: "Paste character",
@@ -158,6 +184,21 @@ function charEntry(ch) {
         ],
     };
 }
+
+function runWithItem(arg) {
+    return loadCategoryData(arg.chain);
+}
+function run() {    
+    return loadCategoryData({chain:[]});
+}
+function runWithString(s) {    
+    // filter all
+    s = s.toUpperCase();
+    return loadAllItems().filter(function (e) {
+	return (e.title.search(s) >= 0 || (e.hasOwnProperty("subtitle") && (e.subtitle.toUpperCase().search(s) >= 0)));
+    });
+}
+
 
 /*
  * Paste the character c
